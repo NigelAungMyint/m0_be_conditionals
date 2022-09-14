919b19d246bdd81ebcdeb9d934dd47e8ec6c46e4
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-09-14 15:14:43 -0500

    Complete Mild Challenge

---
+++ code_challenges/mild.rb
@@ -0,0 +1,15 @@
+## Mild
+
+#Write a Ruby program that defines a variable that stores an Integer.
+#The program should print out the String "even" if the
+#Integer is even, and the String "odd" if the Integer is odd.
+
+int = "chocolate"
+
+if int%2==0
+  puts "even"
+elsif int%2==1
+  puts "odd"
+else
+  puts "neither"
+end
